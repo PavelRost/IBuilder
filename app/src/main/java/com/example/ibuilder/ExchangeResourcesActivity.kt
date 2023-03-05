3e2758ed0f32a77c156813f3c0d64c95849bd517
Rostmp <84409690+Rostmp@users.noreply.github.com>
2023-03-05 21:14:35 +0300

    add: экран обмена ресурсов игрока

--- app/src/main/java/com/example/ibuilder/ExchangeResourcesActivity.kt
+++ app/src/main/java/com/example/ibuilder/ExchangeResourcesActivity.kt
@@ -1,4 +1,18 @@
 package com.example.ibuilder
 
-class ExchangeResourcesActivity {
+import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ExchangeResourcesActivity : AppCompatActivity() {
+
+    private lateinit var textViewNoticeResources: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange_resources)
+        findViewById<RadioGroup>(R.id.radios_exchange).clearCheck()
+        textViewNoticeResources = findViewById(R.id.textView_notice_exchange)
+    }
 }
