f6b979599b2e1c1c2b06fe0301ddf67cb50cfc36
Rostmp <84409690+Rostmp@users.noreply.github.com>
2023-03-06 08:52:52 +0300

    refactor: CostConstruction.kt

--- app/src/main/java/com/example/ibuilder/model/indicatorsDB/CostConstruction.kt
+++ app/src/main/java/com/example/ibuilder/model/indicatorsDB/CostConstruction.kt
@@ -5,13 +5,11 @@ import model.indicatorsDB.TypeResources
 
 object CostConstruction {
 
-    val costConstruction = mutableMapOf<TypeBuilding, Map<TypeResources, Int>>()
-
-    init {
-        costConstruction[TypeBuilding.PRODUCER_GOLD] = mapOf(TypeResources.GOLD to 5)
-        costConstruction[TypeBuilding.PRODUCER_WOOD] = mapOf(TypeResources.WOOD to 3)
-        costConstruction[TypeBuilding.PRODUCER_STONE] = mapOf(TypeResources.GOLD to 3)
-        costConstruction[TypeBuilding.PRODUCER_FOOD] = mapOf(TypeResources.WOOD to 2)
-        costConstruction[TypeBuilding.PRODUCER_WORKER] = mapOf(TypeResources.WOOD to 2)
-    }
+    val costConstruction = mapOf (
+        TypeBuilding.PRODUCER_GOLD to mapOf(TypeResources.GOLD to 5),
+        TypeBuilding.PRODUCER_WOOD to mapOf(TypeResources.WOOD to 3),
+        TypeBuilding.PRODUCER_STONE to mapOf(TypeResources.GOLD to 3),
+        TypeBuilding.PRODUCER_FOOD to mapOf(TypeResources.WOOD to 2),
+        TypeBuilding.PRODUCER_WORKER to mapOf(TypeResources.WOOD to 2)
+    )
 }
